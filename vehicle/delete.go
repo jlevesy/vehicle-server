b77e6829d9a05ebfa068f3138181f8b4186c908f
Julien Levesy <jlevesy@gmail.com>
2024-03-09 20:12:46 +0100

    chore(vehicle): unimplement delete

--- vehicle/delete.go
+++ vehicle/delete.go
@@ -2,9 +2,7 @@ package vehicle
 
 import (
 	"net/http"
-	"strconv"
 
-	"github.com/cicd-lectures/vehicle-server/pkg/httputil"
 	"github.com/cicd-lectures/vehicle-server/storage"
 	"go.uber.org/zap"
 )
@@ -22,33 +20,5 @@ func NewDeleteHandler(store storage.Store, logger *zap.Logger) *DeleteHandler {
 }
 
 func (d *DeleteHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
-	if err != nil {
-		d.logger.Error(
-			"Could not parse vehicle id",
-			zap.Error(err),
-		)
-	}
-
-	ok, err := d.store.Vehicle().Delete(r.Context(), id)
-	if err != nil {
-		d.logger.Error(
-			"Could not delete a vehicle from store",
-			zap.Error(err),
-		)
-
-		httputil.ServeError(rw, http.StatusInternalServerError, err)
-		return
-	}
-
-	if !ok {
-		httputil.ServeJSON(rw, http.StatusNotFound, &httputil.APIError{
-			Code:    httputil.ErrCodeResourceNotFound,
-			Message: "Vehicle not Found",
-		})
-		return
-	}
-
-	rw.WriteHeader(http.StatusNoContent)
-	return
+	http.Error(rw, "Not Implemented", http.StatusInternalServerError)
 }
